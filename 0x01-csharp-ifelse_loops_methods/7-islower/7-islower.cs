00ea85d3f5ce42b4886c0030d5b5a0cb0385be56
sranciato <sranciato1@gmail.com>
2020-04-14 17:45:31 -0400

    [ADD] task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char letter)
+    {
+        if (letter >= 'A' && letter <= 'Z')
+            return false;
+        return true;
+    }
+}
