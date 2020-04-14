4b7acb8491903ba9813045d6a38a2b245be98fee
sranciato <sranciato1@gmail.com>
2020-04-14 17:52:59 -0400

    [ADD] task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last = 0;
+        if (number < 0)
+            number *= -1;
+        last = number % 10;
+        Console.Write(last);
+        return last;
+    }
+}
