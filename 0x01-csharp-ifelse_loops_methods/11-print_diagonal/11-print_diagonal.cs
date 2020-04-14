c3640c34499bbe99359e0440f5878efa801737b8
sranciato <sranciato1@gmail.com>
2020-04-14 18:08:35 -0400

    [ADD] task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length < 1)
+            Console.WriteLine();
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+    }
+}
