b875de128d6cf0f3a34009bc5f1356446067d682
sranciato <sranciato1@gmail.com>
2020-04-16 15:21:18 -0400

    [UPDATE] change for loops from fixed to dynamic length

--- 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -5,9 +5,9 @@ class Matrix
     {
         int[,] newMatrix = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < myMatrix.GetLength(0); i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
             {
                 newMatrix[i,j] = myMatrix[i,j] * myMatrix[i,j];
             }
