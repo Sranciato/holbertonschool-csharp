2861a932e45491f083d76044a556b4ba8a1ec41b
sranciato <sranciato1@gmail.com>
2020-04-16 14:58:45 -0400

    [UPDATE] add edge case for size < 0

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -7,6 +7,11 @@ class List
     {
         List<int> newList = new List<int>();
 
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
         for (int i = 0; i < size; i++)
         {
             newList.Add(i);
