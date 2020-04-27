c58b8de672f7300e616eb612b30777a2d3cd45a9
sranciato <sranciato1@gmail.com>
2020-04-27 16:18:41 -0400

    [ADD] task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList1 = list1.FindAll(elem => !list2.Contains(elem));
+        List<int> newList2 = list2.FindAll(elem => !list1.Contains(elem));
+
+        newList1.AddRange(newList2);
+        newList1.Sort();
+
+        return newList1;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = 0;
+        string topElem = "";
+        bool searchInList = false;
+
+        if (aStack.Count < 1)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            topElem = aStack.Peek();
+        }
+        foreach (string item in aStack)
+        {
+            if (item == search)
+            {
+                searchInList = true;
+            }
+            count += 1;
+        }
+        while (aStack.Pop() != search)
+            ;
+        
+        Console.WriteLine("Number of Items: " + count);
+        Console.WriteLine("Top item: " + topElem);
+        if (!searchInList)
+            Console.WriteLine("Stack contains \"" + search + "\": False");
+        else
+            Console.WriteLine("Stack contains \"" + search + "\": True");
+        
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
