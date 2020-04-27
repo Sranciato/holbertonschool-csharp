cf07b80a36648ce77012627a29be7ef9422ac685
sranciato <sranciato1@gmail.com>
2020-04-27 18:53:20 -0400

    [ADD] task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> searchNode = myLList.First;
+
+        while (searchNode != null)
+        {
+            if (n < searchNode.Value)
+            {
+                myLList.AddBefore(searchNode, n);
+                return searchNode;
+            }
+            searchNode = searchNode.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
