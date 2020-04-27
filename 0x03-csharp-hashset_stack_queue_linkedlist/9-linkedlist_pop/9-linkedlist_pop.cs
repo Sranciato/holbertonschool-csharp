0c12c05993c5c88adee02eefc63700595460e3fc
sranciato <sranciato1@gmail.com>
2020-04-27 18:25:01 -0400

    [ADD] task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count < 1)
+            return 0;
+        LinkedListNode<int> firstNode = myLList.First;
+        myLList.RemoveFirst();
+        return firstNode.Value;
+    }
+}
