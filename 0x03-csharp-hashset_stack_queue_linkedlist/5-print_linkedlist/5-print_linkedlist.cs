08427af5115832ff3e0a66028840ca96de11b76a
sranciato <sranciato1@gmail.com>
2020-04-27 17:24:09 -0400

    [ADD] task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+        }
+        foreach (int item in llist)
+        {
+            Console.WriteLine(item);
+        }
+        return llist;
+    }
+}
