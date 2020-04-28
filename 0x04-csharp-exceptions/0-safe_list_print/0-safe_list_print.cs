764c0b4e71e8e3d875591e01b0d008c13e5fad78
sranciato <sranciato1@gmail.com>
2020-04-28 02:53:04 -0400

    [ADD] task 1:

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+                count += 1;
+                if (count == n)
+                    return count;
+            }
+            return count;
+            
+        }
+        catch (System.Exception)
+        {
+            return count;
+        }
+    }
+}
