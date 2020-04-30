fe93882f0e7fc18837e4d9b05f3d3fb7d9ab239e
sranciato <sranciato1@gmail.com>
2020-04-30 15:07:41 -0400

    [ADD] task 5

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie class that defines Enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health
+        /// </summary>
+        public int health;
+        string name = "(No Name)";
+        /// <summary>
+        /// public constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// public constructor: checks if value is positive
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        /// <summary>
+        /// public property: get and set name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <summary>
+        /// public constructor: returns value of health
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
