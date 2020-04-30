3f56c7923bc7b8a210b4b0741e7e2fdfed3fde63
sranciato <sranciato1@gmail.com>
2020-04-30 15:16:29 -0400

    [ADD] task 6

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie class that defines Enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// private field for name
+        /// </summary>
+        string name = "(No name)";
+        /// <summary>
+        /// public constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// public constructor: checks if value is positive
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        /// <summary>
+        /// public property: get and set name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <summary>
+        /// public constructor: returns value of health
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+        /// <summary>
+        /// public override: override tostring method to print custom string
+        /// </summary>
+        public override string ToString()
+        {
+            string _string = "Zombie name: " + name + " / Total Health: " + health;
+            return _string;
+        }
+    }
+}
