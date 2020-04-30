b5714419596e60e18d04e368fd24033142bf1fcc
sranciato <sranciato1@gmail.com>
2020-04-30 14:47:40 -0400

    [ADD] task 2

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie class that defines Enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// public constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
