070581ecc4fd1c70d0e8b7931a9ee5ee38ea499c
sranciato <sranciato1@gmail.com>
2020-05-05 08:01:47 -0400

    [ADD] task 1

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int a = 3;
+            int b = 4;
+
+            Assert.AreEqual(7, Operations.Add(a, b));
+            Assert.AreNotEqual(9, Operations.Add(a, b));
+            Assert.AreEqual(a + b, Operations.Add(a, b));
+        }
+    }
+}
