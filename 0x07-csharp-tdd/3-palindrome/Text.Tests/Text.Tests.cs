fb4eaabb5225e118802a6f3b9c4e92a5761c7874
sranciato <sranciato1@gmail.com>
2020-05-05 09:34:05 -0400

    [ADD] task 4

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+		public void Test1()
+		{
+			string s = "abcba";
+
+            Assert.AreEqual(true, Str.IsPalindrome(s));
+		}
+        [Test]
+		public void Test2()
+		{
+			string s = "abccba";
+
+            Assert.AreEqual(true, Str.IsPalindrome(s));
+		}
+        [Test]
+		public void Test3()
+		{
+			string s = "abdcba";
+
+            Assert.AreEqual(false, Str.IsPalindrome(s));
+		}
+        [Test]
+		public void Test4()
+		{
+			string s = "aBcbA";
+
+            Assert.AreEqual(true, Str.IsPalindrome(s));
+		}
+        [Test]
+		public void Test5()
+		{
+			string s = "ab,cb  a";
+
+            Assert.AreEqual(true, Str.IsPalindrome(s));
+		}
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Text
+{
+    ///<summary>operations class</summary>
+    public class Str
+    {
+        ///<summary>returns the max integer in a list of integers</summary>
+        public static bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            int end = s.Length - 1;
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (Char.IsLetter(s[i]) == false)
+                    continue;
+                if (Char.IsLetter(s[end]) == false)
+                {
+                    end -= 1;
+                    i -= 1;
+                    continue;
+                }
+                if (s[i] != s[end])
+                    return false;
+                end -= 1;
+            }
+            return true;
+        }
+    }
+}
