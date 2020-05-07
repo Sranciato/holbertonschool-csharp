b6f16fc7bab041286c71446cd2d3a2e657415ba8
sranciato <sranciato1@gmail.com>
2020-05-07 17:55:46 -0400

    [add] task 2

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>returns true if is instance of a class</summary>
+class Obj
+{
+    ///<summary>returns true if is instance of a class</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
