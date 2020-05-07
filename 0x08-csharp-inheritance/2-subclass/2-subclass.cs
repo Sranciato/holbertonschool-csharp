9c5ecbc98a382201ee0b4f76a429a839348ff054
sranciato <sranciato1@gmail.com>
2020-05-07 18:03:40 -0400

    [ADD] task 3

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Obj class</summary>
+class Obj
+{
+    ///<summary>returns true if object is an instance of a class that ingerits from a specified class</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
