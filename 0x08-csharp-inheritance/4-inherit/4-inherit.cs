33be3f3c409b772c6eac64ca4a159a9e20d403b5
sranciato <sranciato1@gmail.com>
2020-05-07 18:51:46 -0400

    [ADD] task 5

--- 0x08-csharp-inheritance/4-inherit/4-inherit.cs
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
-///<summary>Empty base class</summar>
+///<summary>Empty base class</summary>
 class Animal
 {
-    ///<summary>Empty base class</summar>
+
 }
 
 ///<summary>Empty class derived from animal</summary>
 class Dog : Animal
 {
-    ///<summary>Empty class derived from animal</summary>
+
 }
