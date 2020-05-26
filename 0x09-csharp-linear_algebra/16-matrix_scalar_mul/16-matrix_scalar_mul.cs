141b797167add377d58ca266c7a75fb562cdcd56
sranciato <sranciato1@gmail.com>
2020-05-26 01:01:58 -0400

    [aDD] check for indexes

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -11,6 +11,8 @@ class MatrixMath
 
         if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
             return new double[,] {{-1}};
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return new double[,] {{-1}};
 
         int len = matrix.GetLength(0);
         sum = new double[len, len];
