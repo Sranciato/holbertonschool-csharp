c1f624ea4e7ace66a7c48554336898774d777b0e
sranciato <sranciato1@gmail.com>
2020-07-27 17:28:09 -0400

    [ADD] queue

---
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Queue class</summary>
+public class Queue<T>
+{
+    ///<summary>return the queue's type</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
