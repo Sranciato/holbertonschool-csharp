ec58b0cf2bc4362068e9180288fe604fedfc725b
sranciato <sranciato1@gmail.com>
2020-07-28 15:25:46 -0400

    [ADD] abstract base class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Base class</summary>
+abstract class Base
+{
+    public string name = "";
+
+    ///<summary>Override ToString method</summary>
+    public override string ToString()
+    {
+        string _string = name + " is a " + this.GetType().Name;
+        return _string;
+    }
+}
