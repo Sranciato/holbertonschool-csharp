ceba74e2f8bee14166921850d8af5c4b46f08184
sranciato <sranciato1@gmail.com>
2020-07-29 16:51:12 -0400

    [ADD] takedamage and healdamage methods

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+///<summary>Player class</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    ///<summary>delegate to calculate health</summary>
+    public delegate void CalculateHealth(float amount);
+
+    ///<summary>Constructor</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    ///<summary>Print health of player method</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+
+    ///<summary>Take damage method</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            Console.WriteLine(this.name + " takes 0 damage!");
+        else
+            Console.WriteLine(this.name + " takes " + damage + " damage!");
+    }
+
+    ///<summary>heal damage method</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            Console.WriteLine(this.name + " heals 0 HP!");
+        else
+            Console.WriteLine(this.name + " heals " + heal + " HP!");
+    }
+}
